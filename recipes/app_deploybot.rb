a8da040f52954e14df2d64792307e7bf136c1153
Eric Richardson <e@ericrichardson.com>
2015-12-17 08:12:51 -0500

    0.2.4: deploybot - update health check endpoint

--- recipes/app_deploybot.rb
+++ recipes/app_deploybot.rb
@@ -44,7 +44,7 @@ scpr_apps "deploybot" do
         action    :create
         dir       "#{dir}/current"
         hostname  config.hostname
-        path      "/"
+        path      "/check"
         interval  '5s'
       end
     },
