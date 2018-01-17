c91ea7f12ce8d17583b08ede96be3bb5c2e6f4e6
deveon <deveon.fisher@synctree.com>
2018-01-17 12:29:09 -0800

    adding nginx.conf files

--- recipes/_nginx_conf.rb
+++ recipes/_nginx_conf.rb
@@ -2,51 +2,14 @@ template "/etc/nginx/nginx.conf" do
   source  "nginx.conf"
 end
 
-template "/etc/nginx/sites-available/assethost_dev.conf" do
-  source  "assethost_dev.conf"
-end
+FILES=%w(assethost_dev.conf assethost_prod.conf default.conf dev01.conf firetracker_dev.conf firetracker_prod.conf)
+FILES.each do |file|
+  template "/etc/nginx/sites-available/#{file}" do
+    source  "#{file}"
+  end
 
-template "/etc/nginx/sites-available/assethost_prod.conf" do
-  source  "assethost_prod.conf"
-end
-
-template "/etc/nginx/sites-available/default.conf" do
-  source  "default.conf"
-end
-
-template "/etc/nginx/sites-available/dev01.conf" do
-  source  "dev01.conf"
-end
-
-template "/etc/nginx/sites-available/firetracker_dev.conf" do
-  source  "firetracker_dev.conf"
-end
-
-template "/etc/nginx/sites-available/firetracker_prod.conf" do
-  source  "firetracker_prod.conf"
-end
-
-link '/etc/nginx/sites-enabled/assethost_dev.conf' do
-  to '/etc/nginx/sites-available/assethost_dev.conf'
-end
-
-link '/etc/nginx/sites-enabled/assethost_prod.conf' do
-  to '/etc/nginx/sites-available/assethost_prod.conf'
-end
-
-link '/etc/nginx/sites-enabled/default.conf' do
-  to '/etc/nginx/sites-available/default.conf'
-end
-
-link '/etc/nginx/sites-enabled/dev01.conf' do
-  to '/etc/nginx/sites-available/dev01.conf'
-end
-
-link '/etc/nginx/sites-enabled/firetracker_dev.conf' do
-  to '/etc/nginx/sites-available/firetracker_dev.conf'
-end
-
-link '/etc/nginx/sites-enabled/firetracker_prod.conf' do
-  to '/etc/nginx/sites-available/firetracker_prod.conf'
+  link "/etc/nginx/sites-enabled/#{file}" do
+    to "/etc/nginx/sites-available/#{file}"
+  end
 end
 
