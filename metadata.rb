356bce595f62527e22433c5a1041eb884fc560e0
Louise Yang <louise.m.yang@gmail.com>
2018-10-02 23:31:54 -0700

    Adds the missing version 0.2.26

--- metadata.rb
+++ metadata.rb
@@ -7,8 +7,7 @@ license          'All rights reserved'
 description      'Installs/Configures scpr-apps'
 long_description 'Installs/Configures scpr-apps'
 supports         'ubuntu'
-version          '0.2.24'
-
+version          '0.2.26'
 
 depends "apt"
 depends "nginx_passenger", "~> 0.5.5"
