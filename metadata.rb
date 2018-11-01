4b74b1d2cd349b0ba5996b29a07013da3d5d1114
Louise Yang <louise.m.yang@gmail.com>
2018-11-01 10:11:42 -0700

    Deletes audio_ops
    * commits old uncomitted chchchchanges

--- metadata.rb
+++ metadata.rb
@@ -7,18 +7,18 @@ license          'All rights reserved'
 description      'Installs/Configures scpr-apps'
 long_description 'Installs/Configures scpr-apps'
 supports         'ubuntu'
-version          '0.2.32'
+version          '0.2.33'
 
-depends "apt"
-depends "nginx_passenger", "~> 0.5.5"
-depends "lifeguard"
-depends "scpr-consul"
-depends "scpr-tools"
-depends "nfs", "= 2.4.0"
-depends "nodejs"
-depends "python", "~> 1.4.6"
-depends "logrotate"
-depends "scpr-logstash-forwarder"
-depends "runit"
-depends "scpr-java"
-depends "libarchive", "~> 1.0.0"
+depends 'apt'
+depends 'nginx_passenger', '~> 0.5.5'
+depends 'lifeguard'
+depends 'scpr-consul', '~> 0.2.14'
+depends 'scpr-tools'
+depends 'nfs', '= 2.4.0'
+depends 'nodejs'
+depends 'python', '~> 1.4.6'
+depends 'logrotate'
+depends 'scpr-logstash-forwarder'
+depends 'runit'
+depends 'scpr-java'
+depends 'libarchive', '~> 1.0.0'
