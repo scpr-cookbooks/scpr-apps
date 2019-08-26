fcd02906bdc5768ba4b8bdcbfb5035b429d32a26
Louise <louise.m.yang@gmail.com>
2019-08-26 10:52:53 -0700

    Remove scprv4 workers (#9)

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ license          'All rights reserved'
 description      'Installs/Configures scpr-apps'
 long_description 'Installs/Configures scpr-apps'
 supports         'ubuntu'
-version          '0.2.33'
+version          '0.2.34'
 
 depends 'apt'
 depends 'nginx_passenger', '~> 0.5.5'
